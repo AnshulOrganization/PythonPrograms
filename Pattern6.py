c87eb19ad8cd99c723cbe02b6b1bf92d7731e213
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 15:45:13 +0530

    Pattern Problem

---
+++ Pattern6.py
@@ -0,0 +1,16 @@
+"""
+
+         *
+      *  *
+   *  *  *
+*  *  *  *
+
+"""
+
+for i in range(1, 5, 1):
+    for j in range(5-i, 0, -1):
+        print(" ", end="  ")
+
+    for k in range(i, 0, -1):
+        print("*", end="  ")
+    print()
