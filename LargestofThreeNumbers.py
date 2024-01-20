0d1d25bcaef2591be2ce99f71f40138baa54e5a9
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 13:27:34 +0530

    Largest of three numbers

---
+++ LargestofThreeNumbers.py
@@ -0,0 +1,10 @@
+"""
+This is a program to find out Largest of Three Numbers
+"""
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+max_ans = max(a, b, c)
+print(max_ans)
