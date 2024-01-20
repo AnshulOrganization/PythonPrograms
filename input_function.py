bf4df340bbbd6c01048c3754cc2d53f4dbcb5dac
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 12:35:53 +0530

    input function

---
+++ input_function.py
@@ -0,0 +1,15 @@
+"""
+input() takes the input from the user in a String format
+To change it's datatype we need to do type conversion/ Type Casting
+"""
+
+a = input("Enter a: ")
+
+print(type(a))
+# Enter a: 445
+# <class 'str'>
+
+b = int(input("Enter b: "))
+print(type(b))
+# Enter b: 445
+# <class 'int'>
