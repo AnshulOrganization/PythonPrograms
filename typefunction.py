e267abc801799472c5c7b5fa847289c6881b0cc8
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 12:32:01 +0530

    type function in Python

---
+++ typefunction.py
@@ -0,0 +1,26 @@
+"""
+type() tells us the type of data stored in the variable
+"""
+
+a = 10
+b = 10.26
+c = True
+d = "Hello World"
+e = [1, 2, 3]
+f = (1, 2, 3)
+g = {1, 2, 3}
+h = {
+    "name": "Anshul",
+    "age": 18
+}
+i = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
