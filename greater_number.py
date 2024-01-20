b0742433bcbbe163fb26269421796d7a010b2ddd
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 12:53:10 +0530

    Greater Number

---
+++ greater_number.py
@@ -0,0 +1,14 @@
+"""
+This is program to check and print number which is greater.
+"""
+
+a = 150
+b = 20
+c = 5
+
+if (a > b) & (a > c):
+    print("a is greater")
+elif (b > a) & (b > c):
+    print("b is greater")
+else:
+    print("c is greater")
