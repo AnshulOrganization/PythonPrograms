f9a1b6704a88c6069b58edb57cf25de009688a54
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-01-20 12:21:06 +0530

    print function

---
+++ Pythonprintfunction.py
@@ -0,0 +1,16 @@
+# This is a print function
+
+msg = "this is a statement"
+print(msg)
+
+print("Python", "is", "Fun")
+
+# sep=""
+print("1", "2", "3", "4", sep="/")
+print("Monday", "Tuesday", "Wednesday", sep="-")
+
+# end = ""
+msg1 = "Python is fun"
+msg2 = "Python is easy to learn"
+print(msg1, end=", ")
+print(msg2)
